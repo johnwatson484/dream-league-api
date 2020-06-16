405634efa05e800a00149b8534408b110d720984
John Watson <johnwatson484@hotmail.com>
2020-06-16 19:05:01 +0100

    Initial

---
+++ app/plugins/router.js
@@ -0,0 +1,12 @@
+const routes = [].concat(
+  require('../routes/home')
+)
+
+module.exports = {
+  plugin: {
+    name: 'router',
+    register: (server, options) => {
+      server.route(routes)
+    }
+  }
+}
---
+++ app/server.js
@@ -0,0 +1,25 @@
+const hapi = require('@hapi/hapi')
+const config = require('./config')
+
+async function createServer () {
+  // Create the hapi server
+  const server = hapi.server({
+    port: config.port,
+    routes: {
+      validate: {
+        options: {
+          abortEarly: false
+        }
+      }
+    }
+  })
+
+  // Register the plugins
+  await server.register(require('@hapi/inert'))
+  await server.register(require('./plugins/router'))
+  await server.register(require('blipp'))
+
+  return server
+}
+
+module.exports = createServer
