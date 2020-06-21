a9fa9245f9744bf00f413d1e54da3993d71eece6
John Watson <johnwatson484@hotmail.com>
2020-06-21 17:57:55 +0100

    Remove jwt2

--- app/server.js
+++ app/server.js
@@ -17,8 +17,6 @@ async function createServer () {
   // Register the plugins
   await server.register(require('@hapi/inert'))
   await server.register(require('./plugins/logging'))
-  await server.register(require('hapi-auth-jwt2'))
-  await server.register(require('./plugins/token'))
   await server.register(require('./plugins/router'))
   await server.register(require('blipp'))
 
