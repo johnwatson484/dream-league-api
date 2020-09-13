0db6aeb0547d9009108ab32a010366874c8de02e
John Watson <johnwatson484@hotmail.com>
2020-09-13 17:15:31 +0100

    Team autocomplete added

--- app/routes/league/team.js
+++ app/routes/league/team.js
@@ -1,4 +1,6 @@
 const db = require('../../data/models')
+const joi = require('joi')
+const boom = require('@hapi/boom')
 
 module.exports = [{
   method: 'GET',
@@ -8,6 +10,25 @@ module.exports = [{
       return h.response(await db.Team.findAll({
         include: [{ model: db.Division, as: 'division', attributes: ['name'] }],
         order: [['division', 'rank'], ['name']]
+      }))
+    }
+  }
+}, {
+  method: 'POST',
+  path: '/league/teams/autocomplete',
+  options: {
+    validate: {
+      payload: joi.object({
+        prefix: joi.string()
+      }),
+      failAction: async (request, h, error) => {
+        return boom.badRequest(error)
+      }
+    },
+    handler: async (request, h) => {
+      return h.response(await db.Team.findAll({
+        where: { name: { $ilike: request.payload.prefix + '%' } },
+        order: [['name']]
       }))
     }
   }
