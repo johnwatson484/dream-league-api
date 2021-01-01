6178d0583a194bc420816b092f618891af3c0e3a
John Watson <johnwatson484@hotmail.com>
2021-01-01 17:10:03 +0000

    Tests added

--- app/dream-league/results/update.js
+++ app/dream-league/results/update.js
@@ -10,15 +10,17 @@ async function updateConceded (results, resultsDate) {
   const conceded = results.conceded.filter(x => x.conceded > 0)
   for (const concede of conceded) {
     const manager = await db.ManagerKeeper.findOne({ where: { teamId: concede.teamId } })
-    for (let i = 0; i < concede.conceded; i++) {
-      await db.Concede.create({
-        teamId: concede.teamId,
-        gameweekId: results.gameweekId,
-        managerId: manager.managerId,
-        cup: false,
-        created: resultsDate,
-        createdBy: 'results-sheet'
-      })
+    if (manager) {
+      for (let i = 0; i < concede.conceded; i++) {
+        await db.Concede.create({
+          teamId: concede.teamId,
+          gameweekId: results.gameweekId,
+          managerId: manager.managerId,
+          cup: false,
+          created: resultsDate,
+          createdBy: 'results-sheet'
+        })
+      }
     }
   }
 }
@@ -27,15 +29,17 @@ async function updateGoals (results, resultsDate) {
   const goals = results.goals.filter(x => x.goals > 0)
   for (const goal of goals) {
     const manager = await db.ManagerPlayer.findOne({ where: { playerId: goal.playerId } })
-    for (let i = 0; i < goal.goals; i++) {
-      await db.Goal.create({
-        playerId: goal.playerId,
-        gameweekId: results.gameweekId,
-        managerId: manager.managerId,
-        cup: false,
-        created: resultsDate,
-        createdBy: 'results-sheet'
-      })
+    if (manager) {
+      for (let i = 0; i < goal.goals; i++) {
+        await db.Goal.create({
+          playerId: goal.playerId,
+          gameweekId: results.gameweekId,
+          managerId: manager.managerId,
+          cup: false,
+          created: resultsDate,
+          createdBy: 'results-sheet'
+        })
+      }
     }
   }
 }
---
+++ test/integration/results-update.test.js
@@ -0,0 +1,143 @@
+const db = require('../../app/data/models')
+const { update } = require('../../app/dream-league/results')
+const testData = require('../data')
+
+describe('get results input', () => {
+  beforeAll(async () => {
+    await db.Concede.destroy({ truncate: true })
+    await db.Goal.destroy({ truncate: true })
+    await db.ManagerKeeper.destroy({ truncate: true })
+    await db.ManagerPlayer.destroy({ truncate: true })
+    await db.Manager.destroy({ truncate: true })
+    await db.Team.destroy({ truncate: true })
+    await db.Player.destroy({ truncate: true })
+    await db.Manager.bulkCreate(testData.managers)
+    await db.Team.bulkCreate(testData.teams)
+    await db.Player.bulkCreate(testData.players)
+    await db.ManagerKeeper.bulkCreate(testData.managerKeepers)
+    await db.ManagerPlayer.bulkCreate(testData.managerPlayers)
+  })
+
+  afterAll(async () => {
+    await db.Concede.destroy({ truncate: true })
+    await db.Goal.destroy({ truncate: true })
+    await db.ManagerKeeper.destroy({ truncate: true })
+    await db.ManagerPlayer.destroy({ truncate: true })
+    await db.Manager.destroy({ truncate: true })
+    await db.Player.destroy({ truncate: true })
+    await db.Team.destroy({ truncate: true })
+    await db.sequelize.close()
+  })
+
+  beforeEach(async () => {
+    await db.Concede.destroy({ truncate: true })
+    await db.Goal.destroy({ truncate: true })
+  })
+
+  afterEach(async () => {
+    await db.Concede.destroy({ truncate: true })
+    await db.Goal.destroy({ truncate: true })
+  })
+
+  const results = {
+    gameweekId: 1,
+    conceded: [{
+      teamId: 65,
+      conceded: 0
+    }, {
+      teamId: 60,
+      conceded: 1
+    }, {
+      teamId: 28,
+      conceded: 2
+    }, {
+      teamId: 1,
+      conceded: 1
+    }],
+    goals: [{
+      playerId: 2156,
+      goals: 0
+    }, {
+      playerId: 773,
+      goals: 1
+    }, {
+      playerId: 291,
+      goals: 2
+    }, {
+      playerId: 1,
+      goals: 1
+    }]
+  }
+
+  test('should add conceded for team conceding one', async () => {
+    await update(results)
+    const result = await db.Concede.findAll({ where: { teamId: 60 } })
+    expect(result.length).toBe(1)
+  })
+
+  test('should add conceded for team conceding multiple', async () => {
+    await update(results)
+    const result = await db.Concede.findAll({ where: { teamId: 28 } })
+    expect(result.length).toBe(2)
+  })
+
+  test('should not add conceded for team conceding none', async () => {
+    await update(results)
+    const result = await db.Concede.findAll({ where: { teamId: 65 } })
+    expect(result.length).toBe(0)
+  })
+
+  test('should not add conceded for team conceding with no manager', async () => {
+    await update(results)
+    const result = await db.Concede.findAll({ where: { teamId: 1 } })
+    expect(result.length).toBe(0)
+  })
+
+  test('should add gameweek for conceded', async () => {
+    await update(results)
+    const result = await db.Concede.findAll()
+    expect(result.filter(x => x.gameweekId === 1).length).toBe(result.length)
+  })
+
+  test('should add manager for conceded', async () => {
+    await update(results)
+    const result = await db.Concede.findAll({ where: { teamId: 28, managerId: 2 } })
+    expect(result.length).toBe(2)
+  })
+
+  test('should add goal for player scoring one', async () => {
+    await update(results)
+    const result = await db.Goal.findAll({ where: { playerId: 773 } })
+    expect(result.length).toBe(1)
+  })
+
+  test('should add goals for player scoring multiple', async () => {
+    await update(results)
+    const result = await db.Goal.findAll({ where: { playerId: 291 } })
+    expect(result.length).toBe(2)
+  })
+
+  test('should not add goal for player scoring none', async () => {
+    await update(results)
+    const result = await db.Goal.findAll({ where: { playerId: 2156 } })
+    expect(result.length).toBe(0)
+  })
+
+  test('should not add goals for player with no manager', async () => {
+    await update(results)
+    const result = await db.Goal.findAll({ where: { playerId: 1 } })
+    expect(result.length).toBe(0)
+  })
+
+  test('should add gameweek for goal', async () => {
+    await update(results)
+    const result = await db.Goal.findAll()
+    expect(result.filter(x => x.gameweekId === 1).length).toBe(result.length)
+  })
+
+  test('should add manager for goal', async () => {
+    await update(results)
+    const result = await db.Goal.findAll({ where: { playerId: 291, managerId: 3 } })
+    expect(result.length).toBe(2)
+  })
+})
