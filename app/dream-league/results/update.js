c9d76e7c2ba7416b1373a71730a865b9c65560d9
John Watson <johnwatson484@hotmail.com>
2021-01-01 14:55:51 +0000

    Update results

--- app/dream-league/results/update.js
+++ app/dream-league/results/update.js
@@ -3,18 +3,19 @@ const db = require('../../data/models')
 async function update (results) {
   const resultsDate = new Date()
   await updateConceded(results, resultsDate)
-  await updateGoals(results.resultsDate)
+  await updateGoals(results, resultsDate)
 }
 
 async function updateConceded (results, resultsDate) {
   const conceded = results.conceded.filter(x => x.conceded > 0)
   for (const concede of conceded) {
-    const managerId = await db.ManagerGoalKeepers.findOne({ where: { teamId: concede.teamId } })
-    for (let i = 0; i < concede.conceded.length; i++) {
+    const manager = await db.ManagerKeeper.findOne({ where: { teamId: concede.teamId } })
+    for (let i = 0; i < concede.conceded; i++) {
       await db.Concede.create({
-        teamId: conceded.teamId,
-        GameWeekId: results.gameWeekId,
-        managerId,
+        teamId: concede.teamId,
+        gameweekId: results.gameweekId,
+        managerId: manager.managerId,
+        cup: false,
         created: resultsDate,
         createdBy: 'results-sheet'
       })
@@ -25,12 +26,13 @@ async function updateConceded (results, resultsDate) {
 async function updateGoals (results, resultsDate) {
   const goals = results.goals.filter(x => x.goals > 0)
   for (const goal of goals) {
-    const managerId = await db.ManagerPlayers.findOne({ where: { playerId: goal.playerId } })
-    for (let i = 0; i < goal.goals.length; i++) {
+    const manager = await db.ManagerPlayer.findOne({ where: { playerId: goal.playerId } })
+    for (let i = 0; i < goal.goals; i++) {
       await db.Goal.create({
-        playerId: goals.playerId,
-        GameWeekId: results.gameWeekId,
-        managerId,
+        playerId: goal.playerId,
+        gameweekId: results.gameweekId,
+        managerId: manager.managerId,
+        cup: false,
         created: resultsDate,
         createdBy: 'results-sheet'
       })
