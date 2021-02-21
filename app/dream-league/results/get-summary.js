ba91937719ed9b4e8efe0d5fe6fbea7e92fdc037
John Watson <johnwatson484@hotmail.com>
2021-02-21 19:49:41 +0000

    Support no gameweeks

--- app/dream-league/results/get-summary.js
+++ app/dream-league/results/get-summary.js
@@ -2,7 +2,7 @@ const db = require('../../data/models')
 
 async function getSummary (gameweekId) {
   if (!gameweekId) {
-    gameweekId = await db.Summary.max('gameweekId')
+    gameweekId = await db.Summary.max('gameweekId') || 0
   }
   const summary = await db.Summary.findOne({ where: { gameweekId }, raw: true }) || {}
   return summary.summary
