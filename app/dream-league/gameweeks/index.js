ebac01b2009f05e00f043ede9bf0e77cb74ce7fc
John Watson <johnwatson484@hotmail.com>
2021-03-01 16:59:54 +0000

    Remove raw tag

--- app/dream-league/gameweeks/index.js
+++ app/dream-league/gameweeks/index.js
@@ -1,7 +1,7 @@
 const db = require('../../data/models')
 
 async function getCompleted () {
-  return await db.Gameweek.findAll({ include: { model: db.Summary, as: 'summary', attributes: [], required: true }, raw: true })
+  return await db.Gameweek.findAll({ include: { model: db.Summary, as: 'summary', attributes: [], required: true } })
 }
 
 module.exports = getCompleted
