5e7db2e1b8f58511a60463d28bc703b614b6da24
John Watson <johnwatson484@hotmail.com>
2021-08-10 22:05:33 +0100

    Update Helm library chart

--- scripts/sql/add-teams.sql
+++ scripts/sql/add-teams.sql
@@ -1,156 +1,73 @@
-INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (3, 2, 'Coventry City', 'Coventry');
-
+INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (3, 1, 'Coventry City', 'Coventry');
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (5, 1, 'Luton Town', 'Luton');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (6, 2, 'Lincoln City', 'Lincoln');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (7, 3, 'Carlisle United', 'Carlisle');
-
-INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (8, 4, 'Notts County', 'Notts Co');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (9, 2, 'Rotherham United', 'Rotherham');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (10, 1, 'Millwall', 'Millwall');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (11, 2, 'Wycombe Wanderers', 'Wycombe');
-
-INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (12, 1, 'Leeds United', 'Leeds');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (13, 3, 'Scunthorpe United', 'Scunthorpe');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (14, 1, 'Preston North End', 'Preston');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (15, 3, 'Bradford City', 'Bradford');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (16, 2, 'Bolton Wanderers', 'Bolton');
-
-INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (17, 2, 'Bury', 'Bury');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (18, 1, 'Blackburn Rovers', 'Blackburn');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (19, 3, 'Oldham Athletic', 'Oldham');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (20, 2, 'Portsmouth', 'Portsmouth');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (21, 2, 'AFC Wimbledon', 'AFC Wimbledon');
-
-INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (22, 4, 'Norwich City', 'Norwich');
-
-INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (23, 4, 'Yeovil Town', 'Yeovil');
-
-INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (24, 1, 'Charlton Athletic', 'Charlton');
-
+INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (22, 1, 'Fulham', 'Fulham');
+INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (24, 2, 'Charlton Athletic', 'Charlton');
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (25, 3, 'Stevenage', 'Stevenage');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (26, 1, 'Barnsley', 'Barnsley');
-
-INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (27, 2, 'Peterborough United', 'Peterborough');
-
-INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (28, 1, 'Wigan Athletic', 'Wigan');
-
-INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (29, 4, 'Sheffield United', 'Sheff Utd');
-
+INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (27, 1, 'Peterborough United', 'Peterborough');
+INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (28, 2, 'Wigan Athletic', 'Wigan');
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (30, 3, 'Forest Green Rovers', 'Forest Green');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (31, 2, 'Milton Keynes Dons', 'MK Dons');
-
-INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (32, 2, 'Bristol Rovers', 'Bristol Rovers');
-
+INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (32, 3, 'Bristol Rovers', 'Bristol Rovers');
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (33, 1, 'Derby County', 'Derby');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (34, 1, 'Cardiff City', 'Cardiff');
-
-INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (35, 3, 'Crewe Alexandra', 'Crewe');
-
-INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (36, 3, 'Cambridge United', 'Cambridge United');
-
+INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (35, 2, 'Crewe Alexandra', 'Crewe');
+INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (36, 2, 'Cambridge United', 'Cambridge');
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (37, 3, 'Crawley Town', 'Crawley');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (38, 3, 'Port Vale', 'Port Vale');
-
-INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (39, 3, 'Plymouth Argyle', 'Plymouth');
-
+INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (39, 2, 'Plymouth Argyle', 'Plymouth');
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (41, 1, 'Reading', 'Reading');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (42, 1, 'Bristol City', 'Bristol City');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (44, 1, 'Nottingham Forest', 'Nottm Forest');
-
-INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (45, 1, 'Brentford', 'Brentford');
-
-INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (46, 1, 'Sheffield Wednesday', 'Sheff Wed');
-
-INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (47, 2, 'Southend United', 'Southend');
-
-INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (48, 1, 'Hull City', 'Hull');
-
+INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (45, 1, 'Sheffield United', 'Sheff Utd');
+INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (46, 2, 'Sheffield Wednesday', 'Sheff Wed');
+INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (47, 3, 'Sutton United', 'Sutton Utd');
+INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (48, 1, 'Hull City', 'Hull City');
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (49, 1, 'Birmingham City', 'Birmingham');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (50, 1, 'Queens Park Rangers', 'QPR');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (52, 2, 'Gillingham', 'Gillingham');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (53, 2, 'Accrington Stanley', 'Accrington');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (54, 3, 'Northampton Town', 'Northampton');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (55, 2, 'Fleetwood Town', 'Fleetwood');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (56, 3, 'Colchester United', 'Colchester');
-
-INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (57, 3, 'Grimsby Town', 'Grimsby');
-
-INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (58, 2, 'Rochdale', 'Rochdale');
-
+INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (57, 3, 'Hartlepool United', 'Hartlepool');
+INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (58, 3, 'Rochdale', 'Rochdale');
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (59, 2, 'Shrewsbury Town', 'Shrewsbury');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (60, 3, 'Exeter City', 'Exeter');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (61, 3, 'Swindon Town', 'Swindon');
-
-INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (62, 3, 'Cheltenham Town', 'Cheltenham');
-
-INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (63, 2, 'Blackpool', 'Blackpool');
-
+INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (62, 2, 'Cheltenham Town', 'Cheltenham');
+INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (63, 1, 'Blackpool', 'Blackpool');
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (64, 2, 'Doncaster Rovers', 'Doncaster');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (65, 2, 'Sunderland', 'Sunderland');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (66, 1, 'Middlesbrough', 'Middlesbrough');
-
-INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (67, 3, 'Newport County', 'Newport County');
-
+INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (67, 3, 'Newport County', 'Newport Co');
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (68, 2, 'Burton Albion', 'Burton');
-
-INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (69, 3, 'Morecambe', 'Morecambe');
-
+INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (69, 2, 'Morecambe', 'Morecambe');
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (70, 3, 'Mansfield Town', 'Mansfield');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (71, 3, 'Walsall', 'Walsall');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (73, 2, 'Ipswich Town', 'Ipswich');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (74, 2, 'Oxford United', 'Oxford');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (75, 4, 'Unattached', 'Unattached');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (76, 1, 'Stoke City', 'Stoke');
-
-INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (77, 1, 'West Bromich Albion', 'West Brom');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (78, 1, 'Swansea City', 'Swansea');
-
-INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (79, 3, 'Macclesfield', 'Macclesfield');
-
-INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (80, 2, 'Tranmere Rovers', 'Tranmere');
-
-INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (81, 4, 'Aston Villa', 'Aston Villa');
-
-INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (82, 1, 'Fulham', 'Fulham');
-
+INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (80, 3, 'Tranmere Rovers', 'Tranmere');
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (83, 1, 'Huddersfield Town', 'Huddersfield');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (84, 3, 'Salford City', 'Salford');
-
 INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (85, 3, 'Leyton Orient', 'Leyton Orient');
-
+INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (86, 1, 'Bournemouth', 'Bournemouth');
+INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (87, 1, 'West Bromich Albion', 'West Brom');
+INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (88, 3, 'Barrow', 'Barrow');
+INSERT INTO "teams" ("teamId", "divisionId", "name", "alias") VALUES (89, 3, 'Harrogate Town', 'Harrogate');
