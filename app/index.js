495725b655d777c8577f982f8e2cdf7627181fe0
John Watson <john.watson1@defra.gov.uk>
2023-08-27 20:36:14 +0100

    Disable messages

--- app/index.js
+++ app/index.js
@@ -5,13 +5,13 @@ const { start: startServer } = require('./server')
 const { SIGINT, SIGTERM } = require('./constants/signals')
 
 process.on([SIGTERM, SIGINT], async () => {
-  await stopCache()
-  await stopMessaging()
+  // await stopCache()
+  // await stopMessaging()
   process.exit(0)
 })
 
 module.exports = (async () => {
-  await startCache()
-  await startMessaging()
+  // await startCache()
+  // await startMessaging()
   await startServer()
 })()
