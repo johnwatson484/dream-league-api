92514f9b0396f46505d6c2fde7cb1b5c4c477e8e
John Watson <john.watson1@defra.gov.uk>
2023-10-27 22:02:20 +0100

    Add logs

--- app/server/routes/live-scores.js
+++ app/server/routes/live-scores.js
@@ -7,7 +7,6 @@ module.exports = [{
   options: {
     handler: async (request, h) => {
       const matches = await getLiveScores()
-      console.log(matches)
       return h.response(matches)
     }
   }
