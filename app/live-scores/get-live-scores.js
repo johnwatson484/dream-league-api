a1adc88446d530dafde701f4e5a578e3232be3e6
John Watson <john.watson1@defra.gov.uk>
2023-10-27 12:47:15 +0100

    Add logs

--- app/live-scores/get-live-scores.js
+++ app/live-scores/get-live-scores.js
@@ -3,6 +3,7 @@ const { getKeys, get } = require('../cache')
 const getLiveScores = async () => {
   const matches = []
   const matchKeys = await getKeys('live-scores')
+  console.log('Match keys:', matchKeys)
   for (const key of matchKeys) {
     const match = await get('live-scores', key)
     matches.push(match)
