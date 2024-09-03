10e39e0658049cc5914f8252074356ba654648c3
John Watson <john.watson1@defra.gov.uk>
2024-09-03 10:22:33 +0100

    Run without messaging

--- app/messaging/subscribe.js
+++ app/messaging/subscribe.js
@@ -4,22 +4,34 @@ const { updateLiveScores } = require('../live-scores')
 let connection
 let channel
 
-const start = async (aircraft) => {
-  const { host, port, username, password, scoreExchange, scoreQueue } = message
-  connection = await amqp.connect(`amqp://${username}:${password}@${host}:${port}`)
-  channel = await connection.createChannel()
-  await channel.assertExchange(scoreExchange, 'fanout', {
-    durable: true,
-  })
+const start = async () => {
+  try {
+    const { host, port, username, password, scoreExchange, scoreQueue } = message
+    connection = await amqp.connect(`amqp://${username}:${password}@${host}:${port}`)
+    channel = await connection.createChannel()
+    await channel.assertExchange(scoreExchange, 'fanout', {
+      durable: true,
+    })
 
-  const q = await channel.assertQueue(scoreQueue)
+    const q = await channel.assertQueue(scoreQueue)
 
-  await channel.bindQueue(q.queue, scoreExchange, '')
-  console.log('Waiting for score updates')
+    await channel.bindQueue(q.queue, scoreExchange, '')
+    console.log('Waiting for score updates')
 
-  await channel.consume(q.queue, updateLiveScores, {
-    noAck: true,
-  })
+    await channel.consume(q.queue, updateLiveScores, {
+      noAck: true,
+    })
+  } catch (error) {
+    console.error(error)
+    if (channel) {
+      await channel.close()
+    }
+    if (connection) {
+      await connection.close
+    }
+    await new Promise(resolve => setTimeout(resolve, 30000))
+    await start()
+  }
 }
 
 const stop = async () => {
