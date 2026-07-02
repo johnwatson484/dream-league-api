e4040e91fd4cd56079cde8d14cb88ae417d06e95
John Watson <johnwatson484@hotmail.com>
2026-07-02 23:29:53 +0100

    Fix reset-password export and enable bcrypt install scripts

--- app/notifications/send-reset-password.js
+++ app/notifications/send-reset-password.js
@@ -2,10 +2,10 @@ import config from '../config/index.js'
 import { getHtmlStringFromFile } from './get-html-string.js'
 import { sendEmail } from './send-email.js'
 
-const sendResults = async (email, token, userId) => {
+const sendResetPassword = async (email, token, userId) => {
   const link = `${config.webUrl}/reset-password?token=${token}&userId=${userId}`
   const body = getHtmlStringFromFile('reset-password.html', { link })
   await sendEmail(email, 'Dream League - Reset Password', body)
 }
 
-export { sendResults }
+export { sendResetPassword }
